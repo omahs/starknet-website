0812e673c83cbed901296b11afb5085e8cf5c4a4
agongdai <caishaojiang@gmail.com>
2022-12-19 11:21:41 +0800

    navigation first batch

---
+++ src/components/NavigationMenuDropdown.tsx
@@ -0,0 +1,75 @@
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+import cx from "classnames";
+import React, { ReactNode } from 'react';
+
+interface Menu {
+  label: string;
+  url: string;
+  icon?: ReactNode;
+}
+
+interface MenuCategory {
+  category: string;
+  menus: Menu[];
+}
+
+type Props = {
+  title: string;
+  description: string;
+  icon?: ReactNode;
+  mainMenus: MenuCategory[];
+  footerMenus: Menu[];
+};
+
+export const NavigationMenuDropdown = (
+  {
+    title,
+    description,
+    icon,
+    mainMenus,
+    footerMenus,
+  }: Props) => {
+  return (
+    <NavigationMenuPrimitive.Content
+      className={cx(
+        "absolute w-auto top-0 left-0 rounded-lg",
+        "radix-motion-from-start:animate-enter-from-left",
+        "radix-motion-from-end:animate-enter-from-right",
+        "radix-motion-to-start:animate-exit-to-left",
+        "radix-motion-to-end:animate-exit-to-right"
+      )}
+    >
+      <div className="w-[30rem] lg:w-[42rem] p-3">
+        <div className="flex">
+          <i className="mr-2">{icon}</i>
+          <div>
+            <h3 className="text-bodyText text-sm font-medium uppercase mb-3">{title}</h3>
+            <p className="mb-5 text-sm">{description}</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-4 gap-4 pt-5">
+          {mainMenus.map((m, key) => (
+              <div key={key}>
+                <p className="mb-5 text-sm font-medium uppercase">{m.category}</p>
+                <ul>
+                  {m.menus.map((menu, key) => (
+                    <li key={key} className="py-2 mb-2 text-sm"><a href={menu.url}>{menu.label}</a></li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+        </div>
+
+        <div  className="grid grid-rows-2 grid-flow-col gap-4">
+          {footerMenus.map((f, key) => (
+              <div key={key} className="flex items-center">
+                <div className="mr-2">{f.icon}</div>
+                <a href={f.url} className="py-2 mb-2 text-sm">{f.label}</a>
+              </div>
+            ))}
+        </div>
+
+      </div>
+    </NavigationMenuPrimitive.Content>
+  );
+};
--- workspaces/storybook/src/stories/NavigationMenu.stories.tsx
+++ workspaces/storybook/src/stories/NavigationMenu.stories.tsx
@@ -1,7 +1,8 @@
 import { ComponentMeta } from "@storybook/react";
 import React from "react";
 import { NavigationMenu } from "@ui/NavigationMenu";
-import Wrapper from "./Wrapper";
+import { NavigationMenuDropdown } from "@ui/NavigationMenuDropdown";
+import { CheckIcon } from "@radix-ui/react-icons";
 
 export default {
   title: "starknet.io/NavigationMenu",
@@ -12,29 +13,143 @@ export const Primary = () => (
   <div>
     <div
       style={{
-        backgroundColor: "red",
-        padding: 100,
+        padding: 30,
         display: "flex",
-        width: "50%",
       }}
     >
       <NavigationMenu
-        title='LEARNING HUB'
-        description='Start here for an overview of all things Starknet'
-        mainMenus={[
-          {
-            category: 'THE BASICS',
-            menus: [
-              { label: 'What is Starknet?', url: ''}
-            ]
-          }
-        ]}
-        footerMenus={[
-          { label: 'Starknet foundation', url: ''},
-          { label: 'Glossary', url: ''},
-          { label: 'FAQs', url: ''},
-          { label: 'Media kit', url: ''},
-        ]}
+        naviMenus={
+          [
+            {
+              label: 'Learn',
+              dropdown: (
+                <NavigationMenuDropdown
+                  icon={<CheckIcon />}
+                  title='LEARNING HUB'
+                  description='Start here for an overview of all things Starknet'
+                  mainMenus={[
+                    {
+                      category: 'THE BASICS',
+                      menus: [
+                        { label: 'What is Starknet?', url: ''},
+                        { label: 'Relationship with Eth', url: ''},
+                        { label: 'What is the strk (token)?', url: ''},
+                        { label: 'What are stark proofs?', url: ''}
+                      ]
+                    }, {
+                      category: 'HOW STARKNET WORKS',
+                      menus: [
+                        { label: 'ZK rollups', url: ''},
+                        { label: 'Starknet sequencers', url: ''},
+                        { label: 'Starknet provers', url: ''},
+                        { label: 'Concencus mechanism', url: ''},
+                        { label: 'How Eth validates proofs', url: ''}
+                      ]
+                    }, {
+                      category: 'THE FUTURE OF STARKNET',
+                      menus: [
+                        { label: 'Transaction throughput', url: ''},
+                        { label: 'Computation', url: ''},
+                        { label: 'Low fees', url: ''},
+                        { label: 'Fractal scaling', url: ''},
+                        { label: 'Account abstraction', url: ''},
+                        { label: 'Client side proving', url: ''}
+                      ]
+                    }, {
+                      category: 'COMMON QUESTIONS',
+                      menus: [
+                        { label: 'How is Starknet secured?', url: ''},
+                        { label: 'Does Starknet support EVM?', url: ''},
+                        { label: 'What is Cairo?', url: ''},
+                        { label: 'What is Starkex?', url: ''},
+                        { label: 'What will decentralization on Starknet look like?', url: ''}
+
+                      ]
+                    }
+                  ]}
+                  footerMenus={[
+                    { label: 'Starknet foundation', url: '', icon: <CheckIcon />},
+                    { label: 'Glossary', url: '', icon: <CheckIcon />},
+                    { label: 'FAQs', url: '', icon: <CheckIcon />},
+                    { label: 'Media kit', url: '', icon: <CheckIcon />},
+                  ]}
+                />
+              )
+            },
+            {
+              label: 'Developer',
+              dropdown: (
+                <NavigationMenuDropdown
+                  icon={<CheckIcon />}
+                  title='DEVELOPERS HUB'
+                  description='Start here for an overview of all things Starknet'
+                  mainMenus={[
+                    {
+                      category: 'THE BASICS',
+                      menus: [
+                        { label: 'What is Starknet?', url: ''}
+                      ]
+                    }
+                  ]}
+                  footerMenus={[
+                    { label: 'Starknet foundation', url: ''},
+                    { label: 'Glossary', url: ''},
+                    { label: 'FAQs', url: ''},
+                    { label: 'Media kit', url: ''},
+                  ]}
+                />
+              )
+            },
+            {
+              label: 'EcoSystem',
+              dropdown: (
+                <NavigationMenuDropdown
+                  icon={<CheckIcon />}
+                  title='EcoSystem'
+                  description='Start here for an overview of all things Starknet'
+                  mainMenus={[
+                    {
+                      category: 'THE BASICS',
+                      menus: [
+                        { label: 'What is Starknet?', url: ''}
+                      ]
+                    }
+                  ]}
+                  footerMenus={[
+                    { label: 'Starknet foundation', url: ''},
+                    { label: 'Glossary', url: ''},
+                    { label: 'FAQs', url: ''},
+                    { label: 'Media kit', url: ''},
+                  ]}
+                />
+              )
+            },
+            {
+              label: 'Community',
+              dropdown: (
+                <NavigationMenuDropdown
+                  icon={<CheckIcon />}
+                  title='Community'
+                  description='Start here for an overview of all things Starknet'
+                  mainMenus={[
+                    {
+                      category: 'THE BASICS',
+                      menus: [
+                        { label: 'What is Starknet?', url: ''}
+                      ]
+                    }
+                  ]}
+                  footerMenus={[
+                    { label: 'Starknet foundation', url: ''},
+                    { label: 'Glossary', url: ''},
+                    { label: 'FAQs', url: ''},
+                    { label: 'Media kit', url: ''},
+                  ]}
+                />
+              )
+            }
+          ]
+        }
       />
     </div>
   </div>
